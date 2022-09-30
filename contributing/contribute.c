0ae7160bd605f36c8a6b7bbf7c3c530c61837c08
Phil Dexter <phildexter86@gmail.com>
2022-09-30 08:47:22 -0400

    Added good stuff to contribute.c

--- contributing/contribute.c
+++ contributing/contribute.c
@@ -2,5 +2,12 @@
 
 int main(void){
     printf("Welcome to contribute.c!");
+    long long unsigned a =  12L;
+    printf("%llu\n", a);
+    int c = 19;
+    if (c == 19){
+        c = 101919;
+        printf("%d\n", c);
+    }
     return 0;
 }
